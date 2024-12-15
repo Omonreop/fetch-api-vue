fc2c9d25a9add85b3772057f3be63f39ab788808
rafly <rafly.purnomo08@gmail.com>
2024-12-15 12:48:26 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "@/views/AboutView.vue";
+import ProductView from "@/views/ProductView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/product/:id",
+      name: "product",
+      component: ProductView,
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: AboutView,
+    },
+  ],
+});
+
+export default router;
